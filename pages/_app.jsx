efcb7938a562298f307c4cef58a37fde9f27c4b1
Soren Abedi <soren.abedi@gmail.com>
2021-06-03 19:11:54 +0430

    feat: :lipstick: bootstrap global styles
    add fonts, browser reset and simple responsive effect to project
    
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import 'src/assets/scss/global.scss';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
 
 export const Home = () => {
-    return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-            }}
-        >
-            testing
-        </div>
-    );
+    return <>testing</>;
 };
 
 export default Home;
